35b2cd373e54774d10c71bb70ac260a5e071c979
mike dupont <mike.dupont@introspector.local>
2025-01-06 12:00:44 -0500

    now messages are being logged

--- src/controllers/agent-message.ts
+++ src/controllers/agent-message.ts
@@ -1,3 +1,7 @@
+import * as opentelemetry from "@opentelemetry/api"
+import { wrapTracer } from "@opentelemetry/api/experimental"
+const tracer = wrapTracer(opentelemetry.trace.getTracer("agent"))
+
 import { FastifyReply } from "fastify"
 import agentMessageValidator from "@/validators/agent-message"
 import { logger } from "@/lib/logger"
@@ -10,27 +14,25 @@ export const postAgentMessageHandler = async (
 	>,
 	res: FastifyReply,
 ) => {
-	try {
-		console.log({
-			message: "message",
-			query: JSON.stringify(req.query),
-			body: JSON.stringify(req.body),
-			params: JSON.stringify(req.params),
-			headers: JSON.stringify(req.headers),
-			//raw: JSON.stringify(req.raw),
-			id: JSON.stringify(req.id),
-			ip: JSON.stringify(req.ip),
-		})
-	} catch (err) {
-		logger.error("ERROR1", err)
-	}
+	return await tracer.startActiveSpan(
+		"postAgentMessageHandler",
+		async (span: Span) => {
+			span.setAttribute("query", JSON.stringify(req.query))
+			span.setAttribute("body", JSON.stringify(req.body))
+			span.setAttribute("params", JSON.stringify(req.params))
+			span.setAttribute("headers", JSON.stringify(req.headers))
+			span.setAttribute("reid", JSON.stringify(req.id))
+			span.setAttribute("ipip", JSON.stringify(req.ip))
 
-	try {
-		await res.code(200).send([{ text: "Work in progress" }])
-	} catch (err) {
-		logger.error(err)
-		await res.code(500).send({
-			message: "Internal Server Error",
-		})
-	}
+			try {
+				await res.code(200).send([{ text: "Work in progress" }])
+			} catch (err) {
+				logger.error(err)
+				await res.code(500).send({
+					message: "Internal Server Error",
+				})
+			}
+			span.end()
+		},
+	)
 }
